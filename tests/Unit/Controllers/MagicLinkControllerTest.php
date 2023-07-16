dc0b7e4b547864a669ec95dcd7ae6cda00708095
Helder Correia <tuxboy@tlab.pt>
2023-07-16 19:09:04 +0100

    [PT-15] Implement unit test for MagicLinkController

--- tests/Unit/Controllers/MagicLinkControllerTest.php
+++ tests/Unit/Controllers/MagicLinkControllerTest.php
@@ -5,6 +5,7 @@ namespace Unit\Controllers;
 use DG\BypassFinals;
 use PHPUnit\Framework\TestCase;
 use PixelTrack\Controllers\MagicLinkController;
+use PixelTrack\DataTransferObjects\UserTransfer;
 use PixelTrack\RateLimiter\RateLimiter;
 use PixelTrack\Repository\UserRepository;
 use PixelTrack\Service\Config;
@@ -159,6 +160,211 @@ class MagicLinkControllerTest extends TestCase
         $sessionMock->expects(self::once())
             ->method('getFlashBag')
             ->willReturn($flashBagMock);
+
+        $magicLinkController = new MagicLinkController(
+            $mailMock,
+            $configMock,
+            $twigMock,
+            $userRepositoryMock,
+            $rateLimiterMock,
+            $utilityMock
+        );
+
+        $this->assertEquals(
+            new RedirectResponse('/send-magic-link'),
+            $magicLinkController->sendMagicLink($request, $sessionMock)
+        );
+    }
+
+    public function testRequestMagicLinkIsSentAndCreateNewUser(): void
+    {
+        $mailMock = $this->createMock(Mail::class);
+        $configMock = $this->createMock(Config::class);
+        $twigMock = $this->createMock(Twig::class);
+        $userRepositoryMock = $this->createMock(UserRepository::class);
+        $utilityMock = $this->createMock(Utility::class);
+        $sessionMock = $this->createMock(Session::class);
+        $rateLimiterMock = $this->createMock(RateLimiter::class);
+
+        $_ENV['EMAIL_FROM'] = 'noreply@example.com';
+
+        $configMock->expects(self::once())
+            ->method('getBaseUrl')
+            ->willReturn('http://example.com/');
+
+        $userRepositoryMock->expects(self::once())
+            ->method('findUserByEmail')
+            ->with('user@example.com')
+            ->willReturn(null);
+
+        $userRepositoryMock->expects(self::once())
+            ->method('createUserByEmail')
+            ->with('user@example.com');
+
+        $userRepositoryMock->expects(self::once())
+            ->method('regenerateLoginKey')
+            ->with('user@example.com')
+            ->willReturn('user-login-key');
+
+        $request = new Request();
+        $request->server->set('REMOTE_ADDR', '10.0.0.1');
+        $request->request->set('_csrf', 'csrf-token');
+        $request->request->set('email', 'user@example.com');
+
+        $rateLimiterMock->expects(self::once())
+            ->method('check')
+            ->with('10.0.0.1')
+            ->willReturn(true);
+
+        $sessionMock->expects(self::once())
+            ->method('get')
+            ->with('_csrf')
+            ->willReturn('csrf-token');
+
+        $flashBagMock = $this->createMock(FlashBagInterface::class);
+        $flashBagMock->expects(self::once())
+            ->method('add')
+            ->with('success', 'Please verify your mailbox');
+
+        $sessionMock->expects(self::once())
+            ->method('getFlashBag')
+            ->willReturn($flashBagMock);
+
+        $templateWrapperMock = $this->createMock(TemplateWrapper::class);
+        $templateWrapperMock->expects(self::once())
+            ->method('render')
+            ->with([
+                'link' => 'http://example.com/login/user-login-key',
+            ]);
+
+        $environmentMock = $this->createMock(Environment::class);
+        $environmentMock->expects(self::once())
+            ->method('load')
+            ->with('Default/Mail/magic-link.twig')
+            ->willReturn($templateWrapperMock);
+
+        $twigMock->expects(self::once())
+            ->method('getTwig')
+            ->willReturn($environmentMock);
+
+        $mailMock->expects(self::once())
+            ->method('send')
+            ->with([
+                'from' => [
+                    'email' => 'noreply@example.com',
+                    'name' => 'PixelTracks',
+                ],
+                'to' => [
+                    [
+                        'email' => 'user@example.com',
+                        'name' => 'user@example.com',
+                    ]
+                ],
+                'subject' => 'Here is your magic link',
+                'body' => '',
+            ]);
+
+        $magicLinkController = new MagicLinkController(
+            $mailMock,
+            $configMock,
+            $twigMock,
+            $userRepositoryMock,
+            $rateLimiterMock,
+            $utilityMock
+        );
+
+        $this->assertEquals(
+            new RedirectResponse('/send-magic-link'),
+            $magicLinkController->sendMagicLink($request, $sessionMock)
+        );
+    }
+
+    public function testRequestMagicLinkIsSentWithExistingUser(): void
+    {
+        $mailMock = $this->createMock(Mail::class);
+        $configMock = $this->createMock(Config::class);
+        $twigMock = $this->createMock(Twig::class);
+        $userRepositoryMock = $this->createMock(UserRepository::class);
+        $utilityMock = $this->createMock(Utility::class);
+        $sessionMock = $this->createMock(Session::class);
+        $rateLimiterMock = $this->createMock(RateLimiter::class);
+
+        $_ENV['EMAIL_FROM'] = 'noreply@example.com';
+
+        $configMock->expects(self::once())
+            ->method('getBaseUrl')
+            ->willReturn('http://example.com/');
+
+        $userRepositoryMock->expects(self::once())
+            ->method('findUserByEmail')
+            ->with('user@example.com')
+            ->willReturn(new UserTransfer());
+
+        $userRepositoryMock->expects(self::never())
+            ->method('createUserByEmail');
+
+        $userRepositoryMock->expects(self::once())
+            ->method('regenerateLoginKey')
+            ->with('user@example.com')
+            ->willReturn('user-login-key');
+
+        $request = new Request();
+        $request->server->set('REMOTE_ADDR', '10.0.0.1');
+        $request->request->set('_csrf', 'csrf-token');
+        $request->request->set('email', 'user@example.com');
+
+        $rateLimiterMock->expects(self::once())
+            ->method('check')
+            ->with('10.0.0.1')
+            ->willReturn(true);
+
+        $sessionMock->expects(self::once())
+            ->method('get')
+            ->with('_csrf')
+            ->willReturn('csrf-token');
+
+        $flashBagMock = $this->createMock(FlashBagInterface::class);
+        $flashBagMock->expects(self::once())
+            ->method('add')
+            ->with('success', 'Please verify your mailbox');
+
+        $sessionMock->expects(self::once())
+            ->method('getFlashBag')
+            ->willReturn($flashBagMock);
+
+        $templateWrapperMock = $this->createMock(TemplateWrapper::class);
+        $templateWrapperMock->expects(self::once())
+            ->method('render')
+            ->with([
+                'link' => 'http://example.com/login/user-login-key',
+            ]);
+
+        $environmentMock = $this->createMock(Environment::class);
+        $environmentMock->expects(self::once())
+            ->method('load')
+            ->with('Default/Mail/magic-link.twig')
+            ->willReturn($templateWrapperMock);
+
+        $twigMock->expects(self::once())
+            ->method('getTwig')
+            ->willReturn($environmentMock);
+
+        $mailMock->expects(self::once())
+            ->method('send')
+            ->with([
+                'from' => [
+                    'email' => 'noreply@example.com',
+                    'name' => 'PixelTracks',
+                ],
+                'to' => [
+                    [
+                        'email' => 'user@example.com',
+                        'name' => 'user@example.com',
+                    ]
+                ],
+                'subject' => 'Here is your magic link',
+                'body' => '',
+            ]);
 
         $magicLinkController = new MagicLinkController(
             $mailMock,
