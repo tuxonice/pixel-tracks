132f8918eb827bc3863cb944391efc3864fd8f54
Helder Correia <tuxboy@tlab.pt>
2023-07-25 21:14:31 +0100

    [PT-15] Implement unit test for XmlValidator

---
+++ tests/Unit/Validator/XmlValidatorTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Unit\Validator;
+
+use PHPUnit\Framework\TestCase;
+use PixelTrack\Validator\XmlValidator;
+
+class XmlValidatorTest extends TestCase
+{
+    public function testValidDocument(): void
+    {
+        $gpxFile = file_get_contents(dirname(__DIR__, 2) . '/Fixtures/sample.gpx');
+        $gpxSchema = file_get_contents(dirname(__DIR__, 3) . '/src/Schemas/gpx.xsd');
+        $xmlValidator = new XmlValidator();
+
+        self::assertTrue($xmlValidator->isValid($gpxFile, $gpxSchema));
+    }
+}
