462dc7aa6d26917c64005f7450766bc5765b99b5
Helder Correia <tuxboy@tlab.pt>
2026-07-11 19:57:11 +0100

    chore: Make SMTP encryption configurable via DSN query parameter

--- src/Mail/SmtpMailer.php
+++ src/Mail/SmtpMailer.php
@@ -10,17 +10,22 @@ class SmtpMailer implements MailProviderInterface
 {
     public function send(MailMessageTransfer $mailMessageTransfer): bool
     {
-        [$host, $user, $pass, $port] = $this->parseDsn();
+        [$host, $user, $pass, $port, $encryption] = $this->parseDsn();
 
         $mailer = new PHPMailer(true);
         $mailer->SMTPDebug = 0;
         $mailer->isSMTP();
-        $mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mailer->SMTPAutoTLS = true;
         $mailer->Host = $host;
         $mailer->Username = $user;
         $mailer->Password = $pass;
         $mailer->Port = (int)$port;
+
+        match ($encryption) {
+            'smtps' => $mailer->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS,
+            'none' => $mailer->SMTPSecure = '',
+            default => $mailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS,
+        };
+        $mailer->SMTPAutoTLS = $encryption !== 'none';
 
         $fromRecipient = $mailMessageTransfer->getFrom();
         $toRecipient = $mailMessageTransfer->getTo();
@@ -41,15 +46,21 @@ class SmtpMailer implements MailProviderInterface
     private function parseDsn(): array
     {
         $dsn = $_ENV['MAIL_PROVIDER_DSN'];
-        $pattern = '/^smtp:\/\/([a-zA-Z0-9._\-%]+):([a-zA-Z0-9._\-%]+)@([a-zA-Z0-9.\/\-]+):([0-9]+)*$/';
+        $pattern = '/^smtp:\/\/([a-zA-Z0-9._\-%]+):([a-zA-Z0-9._\-%]+)@([a-zA-Z0-9.\/\-]+):([0-9]+)(?:\?(.+))?$/';
 
         if (preg_match($pattern, $dsn, $matches)) {
             $user = urldecode($matches[1]);
             $pass = urldecode($matches[2]);
             $host = $matches[3];
             $port = $matches[4];
+            $encryption = 'starttls';
 
-            return [$host, $user, $pass, $port];
+            if (isset($matches[5])) {
+                parse_str($matches[5], $query);
+                $encryption = $query['encryption'] ?? $encryption;
+            }
+
+            return [$host, $user, $pass, $port, $encryption];
         }
 
         throw new Exception('Unable to parse mail DSN');
